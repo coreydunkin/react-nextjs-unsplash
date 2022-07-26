ed9a124cdb1029899d1e6ae0f61cc6815d8e62db
coreydunkin <corey.dunkin@gmail.com>
2022-07-26 22:56:44 +1000

    Clean up of components.
    Added theming for images in the image gallery component
    
    Fixed Search to be a proper MUI element

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import ImageGallery from "../components/ImageGallery";
 import Search from "../components/Search";
 import useSWRFetch from "../hooks/useSWRFetch";
 import {GalleryDataContext} from "./_app";
+import {ButtonGroup, Button} from "@mui/material";
+import {Grid} from "@mui/material";
 
 export default function Home() {
   const { query, page, limit, setPage } = useContext(GalleryDataContext);
@@ -10,15 +12,21 @@ export default function Home() {
   console.log(data)
   return (
       <div>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={10}>
+            <Search />
+          </Grid>
+          <Grid item xs={12} md={2}>
+            {query !== null &&
+              <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                <Button disabled={page === 1} onClick={() => setPage(page > 1 ? page - 1 : 1)}>Previous</Button>
+                <Button onClick={() => setPage(page + 1)}>Next</Button>
+              </ButtonGroup>
+            }
+          </Grid>
+        </Grid>
 
-        <Search />
-        <p>Show us the {query} query</p>
-        {query !== null &&
-          <>
-            <button onClick={() => setPage(page > 1 ? page - 1 : 1)}>decrement</button>
-            <button onClick={() => setPage(page + 1)}>Increment</button>
-          </>
-        }
+
 
         {/*{isLoading && <Loading />}*/}
         {/*{isError && <Error />}*/}
