866b11a6d83d8125c5c071b3f6a0278794ec752c
coreydunkin <corey.dunkin@gmail.com>
2022-07-26 22:56:44 +1000

    Clean up of components.
    Added theming for images in the image gallery component
    
    Fixed Search to be a proper MUI element

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,51 +21,51 @@ export default function Home() {
   if(loading) return <div>Loading...</div>
 
   return (
+    <div>
+
+      <Search handleSubmit={handleSubmit} import {useContext} from 'react';
+      import ImageGallery from '../components/ImageGallery';
+      import Search from '../components/Search';
+      import {GalleryDataContext} from '../providers/GalleryDataProvider';
+      import {Grid} from '@mui/material';
+      import PaginationItem from '../components/PaginationItem';
+      import Error from '../components/Error';
+      import Loading from '../components/Loading';
+
+      export default function Home() {
+      const {data, loading, error} = useContext(GalleryDataContext);
+      return (
       <div>
-
-        <Search handleSubmit={handleSubmit} import {useContext} from 'react';
-        import ImageGallery from '../components/ImageGallery';
-        import Search from '../components/Search';
-        import {GalleryDataContext} from '../providers/GalleryDataProvider';
-        import {Grid} from '@mui/material';
-        import PaginationItem from '../components/PaginationItem';
-        import Error from '../components/Error';
-        import Loading from '../components/Loading';
-
-        export default function Home() {
-        const {data, loading, error} = useContext(GalleryDataContext);
-        return (
-        <div>
-        <h1>NEXTSPLASH 😎</h1>
-        <Grid container spacing={2}>
-        <Grid item xs={12} md={8}>
-        <Search />
-        </Grid>
-        <Grid item xs={12} md={4}>
-        <PaginationItem />
-        </Grid>
-        </Grid>
-      {loading && <Loading />}
-      {error && <Error />}
-      {data && <ImageGallery data={data.results}/>}
-        </div>
-        )
-      }handleClear={handleClear} />
-        <p>Show us the {query} query</p>
-        {query !== null &&
-          <>
-            <button onClick={() => setPage(page > 1 ? page - 1 : 1)}>decrement</button>
-            <button onClick={() => setPage(page + 1)}>Increment</button>
-          </>
-        }
-
-        {page}
-        {/*{isLoading && <Loading />}*/}
-        {/*{isError && <Error />}*/}
-        {data && <ImageGallery data={data}/>}
-
-        {/*{JSON.stringify(data)}*/}
-
+      <h1>NEXTSPLASH 😎</h1>
+      <Grid container spacing={2}>
+      <Grid item xs={12} md={8}>
+      <Search />
+      </Grid>
+      <Grid item xs={12} md={4}>
+      <PaginationItem />
+      </Grid>
+      </Grid>
+    {loading && <Loading />}
+    {error && <Error />}
+    {data && <ImageGallery data={data.results}/>}
       </div>
-    )
+      )
+    }handleClear={handleClear} />
+      <p>Show us the {query} query</p>
+      {query !== null &&
+        <>
+          <button onClick={() => setPage(page > 1 ? page - 1 : 1)}>decrement</button>
+          <button onClick={() => setPage(page + 1)}>Increment</button>
+        </>
+      }
+
+      {page}
+      {/*{isLoading && <Loading />}*/}
+      {/*{isError && <Error />}*/}
+      {data && <ImageGallery data={data}/>}
+
+      {/*{JSON.stringify(data)}*/}
+
+    </div>
+  )
 }
